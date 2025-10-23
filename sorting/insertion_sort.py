5b280bb44168f083143e0c7e4c5b576979956102
Arjun M S <64315213+arjun-ms@users.noreply.github.com>
2025-10-23 10:02:14 +0530

    Update insertion_sort.py

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -3,14 +3,14 @@ def insertionSort(nums):
         n = len(nums)
         for i in range(1,n):
             curr = nums[i]
-            prev = i-1
+            j = i-1
 
-            while prev >= 0 and nums[prev] > curr:
+            while j >= 0 and nums[j] > curr:
                 # shift all element to right
-                nums[prev+1] = nums[prev]
-                prev -= 1
+                nums[j+1] = nums[j]
+                j -= 1
 
-            nums[prev+1] = curr # placing the curr (card) in correct position
+            nums[j+1] = curr # placing the curr (card) in correct position
 
         return nums
         
