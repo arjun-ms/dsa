db7bac475eaf27f3974f3058f434f28d76f8460b
Arjun M S <64315213+arjun-ms@users.noreply.github.com>
2025-10-28 14:48:51 +0530

    Implement search in rotated sorted array

---
+++ search_in_rotated_sorted_array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums, k):
+        low = 0
+        high = len(nums) - 1
+
+        while low<=high:
+            mid = (low+high)//2
+
+            # check middle element
+            if k == nums[mid]:
+                return mid
+                
+            # Check if Left half is sorted
+            if(nums[low]<=nums[mid]):
+                # search in the left half,
+                if nums[low] <= k and k <= nums[mid]:
+                    high = mid - 1
+                # else search in right half
+                else:
+                    low = mid + 1  
+
+            # Check if Right half is sorted
+            else:
+                if nums[mid] <= k and k <= nums[high]:
+                    low = mid + 1 # search in right half
+                else:
+                    high = mid - 1 # search in left half
+
+        return -1 
